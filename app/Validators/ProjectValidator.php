58ae7841ed6942276529d11ec2d089d1f9046135
Marcelo <azv@iMac-de-Marcelo.local>
2015-09-16 10:04:38 -0300

    Cap Relacionando Models - Criando ProjectService | Criando ProjectNote

---
+++ app/Validators/ProjectValidator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azv
+ * Date: 08/09/15
+ * Time: 15:21
+ */
+
+namespace CodeProject\Validators;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class ProjectValidator extends LaravelValidator
+{
+    protected $rules = [
+        'owner_id' => 'required|integer',
+        'client_id' => 'required|integer',
+        'name' => 'required',
+        'progress' => 'required',
+        'status' => 'required',
+        'due_date' => 'required'
+    ];
+
+}
